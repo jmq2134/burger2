9d0f758310dfe6a35d9f3e94e95e490b0fac85b0
Julie Quinn <jmq2134@gmail.com>
2017-08-14 11:51:59 -0400

    everything working but burgers/update

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Gourdon-Hamsey Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Poutine on the Ritz Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Lets Give Em Something Shiitake Bout Burger', false);
