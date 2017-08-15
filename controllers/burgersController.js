a831baaf85495ceb1a8ef38a254b11df2bba018b
Julie Quinn <jmq2134@gmail.com>
2017-08-14 22:04:14 -0400

    all working

--- controllers/burgersController.js
+++ controllers/burgersController.js
@@ -1,11 +1,14 @@
+// REQUIRE FILES
 var express = require('express');
 var router = express.Router();
 var models = require('../models');
 
+// REROUTE TO /BURGERS
 router.get('/', function(req, res) {
     res.redirect('/burgers');
 });
 
+// GET ALL BURGERS
 router.get('/burgers', function(req, res) {
     models.burger.findAll().then(function(data) {
         var hbsObject = { burgers: data };
@@ -13,6 +16,7 @@ router.get('/burgers', function(req, res) {
     });
 });
 
+// CREATE A NEW BURGER
 router.post('/burgers/create', function(req, res) {
     models.burger.create({
         burger_name: req.body.burger_name,
@@ -22,8 +26,8 @@ router.post('/burgers/create', function(req, res) {
     });
 });
 
+// UPDATE DEVOUR PROPERTY
 router.post('/burgers/update/:id', function(req, res) {
-
     models.burger.update({
         devoured: req.body.devoured
     }, { where: { id: req.params.id } }).then(function(data) {
@@ -33,12 +37,12 @@ router.post('/burgers/update/:id', function(req, res) {
     });
 });
 
+// DELETE BURGERS
 router.delete('/burgers/delete/:id', function(req, res) {
-    models.burger.destroy({
-        where: { id: req.params.id }
-    }).then(function() {
+    models.burger.destroy({ where: { id: req.params.id }}).then(function() {
         res.redirect('/burgers');
     });
 });
 
+// EXPORT ROUTER
 module.exports = router;
